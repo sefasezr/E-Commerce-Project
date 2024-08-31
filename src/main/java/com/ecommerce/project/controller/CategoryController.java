b2c99bf66f32d86201572468b8d342d2ffdfce66
sefasezr <153550790+sefasezr@users.noreply.github.com>
2024-09-01 01:56:09 +0300

    1.09.2024

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.ecommerce.project.controller;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +23,12 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<Category>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -18,6 +19,19 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if (category == null) {
+            return "Category not found";
+        }
+        categories.remove(category);
+        return "Category with categoryId: "+ categoryId + "deleted successfully !!";
     }
 }
